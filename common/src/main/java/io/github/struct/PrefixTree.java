14e204c639e5877dedc98e62ae0e8f14d0720827
whimthen <whimthen@gmail.com>
2020-03-23 17:57:47 +0800

    Add: PrefixTree field

--- common/src/main/java/io/github/struct/PrefixTree.java
+++ common/src/main/java/io/github/struct/PrefixTree.java
@@ -1,12 +1,37 @@
 package io.github.struct;
 
-public class PrefixTree {
+import java.io.Serializable;
+import java.util.Map;
 
+public class PrefixTree implements Serializable {
 
-	public class Node {
+	private Node              node;
+	private Map<String, Node> children;
+
+	public PrefixTree(Node node) {
+		this.node = node;
+	}
+
+	public void addChild(Node node) {
+		this.children.put(node.getPath(), node);
+	}
+
+	public Node getChild(String path) {
+		return children.get(path);
+	}
+
+	public Node getNode() {
+		return node;
+	}
+
+	public void setNode(Node node) {
+		this.node = node;
+	}
+
+	public static class Node {
 //		private Node node;
-		private NodeType nType;
-		private String path;
+		private NodeType type;
+		private String   path;
 
 		/**
 		 * 自节点索引开头字母组合
@@ -23,9 +48,49 @@ public class PrefixTree {
 		 * 是否是通配符节点
 		 */
 		private boolean catchAll;
+
+		public NodeType getType() {
+			return type;
+		}
+
+		public void setType(NodeType type) {
+			this.type = type;
+		}
+
+		public String getPath() {
+			return path;
+		}
+
+		public void setPath(String path) {
+			this.path = path;
+		}
+
+		public String getIndices() {
+			return indices;
+		}
+
+		public void setIndices(String indices) {
+			this.indices = indices;
+		}
+
+		public boolean isWildChild() {
+			return wildChild;
+		}
+
+		public void setWildChild(boolean wildChild) {
+			this.wildChild = wildChild;
+		}
+
+		public boolean isCatchAll() {
+			return catchAll;
+		}
+
+		public void setCatchAll(boolean catchAll) {
+			this.catchAll = catchAll;
+		}
 	}
 
-	public enum NodeType {
+	public static enum NodeType {
 		STATIC, // 非根节点的普通字符串节点
 		ROOT, // 根节点
 		PARAM, // 参数节点, 例如 :id
