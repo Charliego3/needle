20812e4f20e33cdd1dec1e57c497bdf88b1e1115
whimthen <whimthen@gmail.com>
2020-03-26 18:25:08 +0800

    Update: change nullDefault methods if object is array and array's length equals 0 return default value or supplier.get()

--- common/src/main/java/io/github/kits/Objs.java
+++ common/src/main/java/io/github/kits/Objs.java
@@ -1,16 +1,19 @@
 package io.github.kits;
 
+import java.lang.reflect.Array;
 import java.util.Objects;
 import java.util.function.Supplier;
 
 public class Objs {
 
 	public static <T> T nullDefault(T object, T defaultValue) {
-		return Objects.isNull(object) ? defaultValue : object;
+		return nullDefault(object, () -> defaultValue);
 	}
 
 	public static <T> T nullDefault(T object, Supplier<T> supplier) {
-		return Objects.isNull(object) ? supplier.get() : object;
+		return Objects.isNull(object) ||
+				   (object.getClass().isArray() && Array.getLength(object) == 0)
+				   ? supplier.get() : object;
 	}
 
 	public static boolean isAllNull(Object... objects) {
